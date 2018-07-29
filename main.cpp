a6fba5ae62cbf39cc9543422c20d781a83b62193
root <root@localhost.localdomain>
2018-07-29 15:26:21 -0400

    second commit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #define ETHER_ADDR_LEN 6
 #define ETHERTYPE_IP 0x800
 #define ETHERTYPE_ARP 0x806
+#define ETHER_IP_OFFSET 14
 struct ethernet{
 	u_char ether_dhost[ETHER_ADDR_LEN];
 	u_char ether_shost[ETHER_ADDR_LEN];
@@ -29,28 +30,31 @@ struct ipH{
 };
 
 struct tcp{
-	uint16_t tcp_src;
-	uint16_t tcp_dst;
-	uint32_t tcp_seq_num;
-	uint32_t tcp_ack_num;
-	uint8_t tcp_res1:4;
-	uint8_t tcp_hdr_len:4;
-	uint8_t tcp_fin:1;
-	uint8_t tcp_syn:1;
-	uint8_t tcp_rst:1;
-	uint8_t tcp_psh:1;
-	uint8_t tcp_ack:1;
-	uint8_t tcp_urg:1;
-	uint8_t tcp_res2:2;
-	uint16_t tcp_win_size;
-	uint16_t tcp_chk;
-	uint16_t tcp_urg_ptr;
+	u_short th_sport;	/* source port */
+	u_short th_dport;	/* destination port */
+	u_int th_seq;		/* sequence number */
+	u_int th_ack;		/* acknowledgement number */
+	u_char th_off;	/* data offset, rsvd */
+#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
+	u_char th_flags;
+#define TH_FIN 0x01
+#define TH_SYN 0x02
+#define TH_RST 0x04
+#define TH_PUSH 0x08
+#define TH_ACK 0x10
+#define TH_URG 0x20
+#define TH_ECE 0x40
+#define TH_CWR 0x80
+#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+	u_short th_win;		/* window */
+	u_short th_sum;		/* checksum */
+	u_short th_urp;		/* urgent pointer */
 };
 
 u_int16_t show_ether_header(struct ethernet *);
 u_char show_ip_header(struct ipH *);
 u_int8_t show_tcp_header(struct tcp * );
-void show_data(u_char *);
+void show_data(u_char *,int,int,int);
 void print_mac_addr(u_char * mac_addr);
 
 void usage() {
@@ -78,7 +82,7 @@ int main(int argc, char* argv[]) {
 		struct ethernet *ether_header;
 		struct ipH * ip_header;
 		struct tcp * tcp_header;
-		int offset = 0;
+		int total_len = 0, ip_tcp_offset = 0, tcp_data_offset = 0;
 		int res = pcap_next_ex(handle, &header, &packet);
 		if (res == 0) continue;
 		if (res == -1 || res == -2) break;
@@ -86,16 +90,17 @@ int main(int argc, char* argv[]) {
 		ether_header = (struct ethernet*)(packet);
 		if(show_ether_header(ether_header) == ETHERTYPE_IP)
 		{
-			ip_header = (struct ipH *)(packet+14);
-			offset = ip_header->ip_header_len * 4;
+			ip_header = (struct ipH *)(packet+ETHER_IP_OFFSET);
+			ip_tcp_offset = ip_header->ip_header_len * 4;
+			total_len = ip_header->ip_total_length;
 			if(show_ip_header(ip_header) == IPPROTO_TCP)
 			{		
-				tcp_header = (struct tcp *)(ip_header+offset);
-				offset = tcp_header->tcp_hdr_len * 4;
+				tcp_header = (struct tcp *)((u_char *)ip_header+ip_tcp_offset);
+				tcp_data_offset = tcp_header->th_off * 4;
 				show_tcp_header(tcp_header);
-				if(offset > 0)
+				if(tcp_data_offset > 0)
 				{
-					show_data((u_char *)(tcp_header+offset));
+					show_data((u_char *)tcp_header+tcp_data_offset,total_len,ip_tcp_offset,tcp_data_offset);
 				}
 			}
 		}
@@ -133,17 +138,18 @@ u_char show_ip_header(struct ipH * ip_header)
 
 uint8_t show_tcp_header(struct tcp * tcp_header)
 {
-	printf("[-] Source Port : %d\n", ntohs(tcp_header->tcp_src));
-	printf("[-] Dest Port :%d\n", ntohs(tcp_header->tcp_dst));
-	return tcp_header->tcp_hdr_len;
+	printf("[-] Source Port : %d\n", ntohs(tcp_header->th_sport));
+	printf("[-] Dest Port :%d\n", ntohs(tcp_header->th_dport));
+	return tcp_header->th_off;
 }
 
-void show_data(u_char * data)
+void show_data(u_char * data,int total_len,int ip_tcp_offset,int tcp_data_offset)
 {
-	for(int i = 0;i<sizeof(data);i++)
+	int size = total_len-ETHER_IP_OFFSET-ip_tcp_offset-tcp_data_offset;
+	for(int i = 0;i<size;i++)
 	{
 		if(i>16) break;
-		printf("%02c ",data+i);
+		printf("%c ",data[i]);
 	}
 	printf("\n");
 }
